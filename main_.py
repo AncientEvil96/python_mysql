b82df82644d3e75c52770209c2e532020eeb9147
AncientEvil96 <bersek123@gmail.com>
2021-03-05 16:37:41 +0300

    update main

--- main_.py
+++ main_.py
@@ -4,39 +4,43 @@ import psycopg2
 def pssql_qvery(conn, qwery):
     cursor = conn.cursor()
     cursor.execute(qwery)
-    records = cursor.fetchall()
+    conn.commit()
+    # records = cursor.fetchall()
 
-    print(cursor.description)
-    for row in records:
-        print(row)
+    # print(cursor.description)
+    # for row in records:
+    #     print(row)
 
-    cursor.close()
+    # cursor.close()
 
 
 def mysql_connect():
-    conn = psycopg2.connect(dbname='order_swap', user='postgres',
+    conn = psycopg2.connect(dbname='order_swap_test', user='postgres',
                             password='delay159', host='192.168.0.175')
     return conn
 
 
 if __name__ == "__main__":
     conn = mysql_connect()
-
+    i = 0
     try:
-        qwery = """
-        COPY orders (id, added_at, departure_at, code_shop, franch_inn, weight, volume, width, depth, height)
-        FROM '\\\\192.168.0.198\\tbp\\temp_order_csv_test\\orders\\1_SQL_CSV_TBP_orders_20210304164247.csv'
-        WITH (FORMAT csv,
-        HEADER TRUE,
-        DELIMITER '|'
-        );
-        """
-        qwery = qwery.replace('\\', '\\\\')
+        while i < 20:
+            i += 1
+            qwery = """
+            COPY orders (id, added_at, departure_at, code_shop, franch_inn, weight, volume, width, depth, height)
+            FROM '\\\\192.168.0.198\\tbp\\temp_order_csv_test\\orders\\1_SQL_CSV_TBP_orders_20210304164247.csv'
+            WITH (FORMAT csv,
+            HEADER TRUE,
+            DELIMITER '|'
+            );
+            """
+            qwery = qwery.replace('\\', '\\\\')
 
-        # qwery = 'select * from orders'
+            # qwery = 'select * from orders'
 
-        # qwery = 'select version();'
-        pssql_qvery(conn, qwery)
+            # qwery = 'select version();'
+            pssql_qvery(conn, qwery)
+            print(f'iteraion {i}')
     except Exception as err:
         print(f'error: {err}')
     else:
